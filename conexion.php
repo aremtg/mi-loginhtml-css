b387fc222ea54e4d2bd58ba2f1b859a8aa6cf6a9
tatiana <tgz57031@gmail.com>
2023-05-19 11:52:19 -0500

    m

--- conexion.php
+++ conexion.php
@@ -2,18 +2,19 @@
 $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
-$db = "homerbook";
+$db_login = "login";
+$db_registro = "registro"
 
-$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $db);
+$conn1 = mysqli_connect($dbhost, $dbuser, $dbpass, $db_login);
 
-if(!$conn){
+if(!$conn1){
     die("no hay conexion tatis: ".mysqli_connect_error());
 }
 $usuario_ingresado = $_POST["usuario"];
 $contrasena_ingresada = $_POST["contrasena"];
 
 
-$query = mysqli_query($conn, "SELECT * FROM usuarios WHERE nombre = '".$usuario_ingresado."' and codigo = '".$contrasena_ingresada."'");
+$query = mysqli_query($conn1, "SELECT * FROM usuarios WHERE nombre = '".$usuario_ingresado."' and codigo = '".$contrasena_ingresada."'");
 $filaDeArriba = mysqli_num_rows($query);
 
 if($filaDeArriba == 1){
@@ -22,4 +23,7 @@ if($filaDeArriba == 1){
     header("location: login.html");
     // echo "error tatiana";
 }
+
+
+
 ?>
