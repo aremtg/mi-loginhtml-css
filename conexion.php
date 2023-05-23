3ae8c8500aa3d225282792cf23b2bca4c12acc07
tatiana <tgz57031@gmail.com>
2023-05-23 11:41:23 -0500

    mm

--- conexion.php
+++ conexion.php
@@ -2,19 +2,18 @@
 $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
-$db_login = "login";
-$db_registro = "registro"
+$db_login = "homerbook";
 
-$conn1 = mysqli_connect($dbhost, $dbuser, $dbpass, $db_login);
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $db_login);
 
-if(!$conn1){
-    die("no hay conexion tatis: ".mysqli_connect_error());
+if(!$conn){
+    die("no hay conexion tat: ".mysqli_connect_error());
 }
 $usuario_ingresado = $_POST["usuario"];
 $contrasena_ingresada = $_POST["contrasena"];
 
 
-$query = mysqli_query($conn1, "SELECT * FROM usuarios WHERE nombre = '".$usuario_ingresado."' and codigo = '".$contrasena_ingresada."'");
+$query = mysqli_query($conn, "SELECT * FROM usuarios WHERE nombre = '".$usuario_ingresado."' and codigo = '".$contrasena_ingresada."'");
 $filaDeArriba = mysqli_num_rows($query);
 
 if($filaDeArriba == 1){
